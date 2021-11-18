b86879fd57e693b0ed5178ccc242ebcdc8ca0647
Igor Klyopov <shoot8202@gmail.com>
2021-11-18 09:24:07 +0200

    add basic files of project

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import './common/cssVariables.css';
+import './common/globalStyles.css';
+
+function App() {
+  return <h1>Hello everyone!</h1>;
+}
+
+export default App;
---
+++ src/components/ButtonCross/ButtonCross.js
@@ -0,0 +1,5 @@
+import style from './name.module.css';
+
+export default function name() {
+  return ()
+}
